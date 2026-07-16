cb3632ef6f96bf66b37034c5179124fa2357372e
raton_kumar <ratankumarppi86@gmail.com>
2026-07-16 22:39:54 +0600

    updated

--- resources/views/webview/content/product/single/feedback.blade.php
+++ resources/views/webview/content/product/single/feedback.blade.php
@@ -17,14 +17,11 @@
             {!! $feedback['feedback_title'] ?? 'আমাদের সম্পর্কে গ্রাহকদের মতামত' !!}
         </h2>
         
-        <p style="font-size: 18px; color: rgb(138, 130, 120); margin-bottom: 40px;">
+        
             {!! $feedback['feedback_subtitle'] ?? 'যাঁরা আমাদের ওপর আস্থা রেখেছেন' !!}
-        </p>
 
         <div style=" padding: 40px; border-radius: 24px;box-shadow: 0 10px 30px rgba(0,0,0,0.03);">
-            <p style="font-size: 20px; line-height: 1.8; color: rgb(30, 26, 21); font-style: italic; margin-bottom: 24px;">
-                "{!! $feedback['feedback_descraption'] ?? 'এখানে গ্রাহকের মন্তব্যটি প্রদর্শিত হবে...' !!}"
-            </p>
+            "{!! $feedback['feedback_descraption'] ?? 'এখানে গ্রাহকের মন্তব্যটি প্রদর্শিত হবে...' !!}"
             
             <div style="font-weight: 700; font-size: 16px; color: rgb(240, 83, 43);">
                 — {!! $feedback['feedback_author'] ?? 'গ্রাহকের নাম' !!}
