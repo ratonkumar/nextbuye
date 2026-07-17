c669f8c19bb2b090e1dba9112fe49712ade0bdd8
raton_kumar <ratankumarppi86@gmail.com>
2026-07-17 12:20:22 +0600

    product comparision update

--- resources/views/webview/content/product/single/comparison.blade.php
+++ resources/views/webview/content/product/single/comparison.blade.php
@@ -3,33 +3,34 @@
     ->where('product_id', $productdetails->id)
     ->where('is_active', 1)
     ->first();  
-    $data = $productComparison ? json_decode($productComparison->content, true) : null;
+    $productComparisondata = $productComparison ? json_decode($productComparison->content, true) : null;
+
 @endphp
 
-@if($data)
+@if($productComparisondata)
 <section style="max-width: 940px; margin: 0px auto; padding: 80px 20px 20px">
    
-        {!! $data['product_comparison_title'] !!}
+        {!! $productComparisondata['product_comparison_title'] !!}
     <p style="text-align: center; font-size: 16.5px; color: rgb(107, 100, 90); max-width: 560px; margin: 0px auto 36px; line-height: 1.75;">
-        {!! $data['product_comparison_description'] !!}
+        {!! $productComparisondata['product_comparison_description'] !!}
     </p>
 
     <div style="display: flex; flex-wrap: wrap; gap: 18px; align-items: stretch; justify-content: center">
         
         <!-- Left Column (Generic) -->
         <div style="flex: 1 1 300px; max-width: 430px; background: rgb(247, 242, 234); border: 1px solid rgb(232, 224, 212); border-radius: 24px; padding: 26px 24px;">
-            {!! $data['product_comparison_left'] !!}
+            {!! $productComparisondata['product_comparison_left'] !!}
         </div>
 
         <!-- Right Column (Choplet) -->
         <div style="flex: 1 1 300px; max-width: 430px; background: rgb(255, 255, 255); border: 2px solid rgb(240, 83, 43); border-radius: 24px; padding: 26px 24px; box-shadow: rgba(240, 83, 43, 0.55) 0px 26px 50px -30px; position: relative;">
-             {!! $data['product_comparison_right'] !!}
+             {!! $productComparisondata['product_comparison_right'] !!}
         </div>
     </div>
 
     <!-- Bottom Text -->
     <div style="max-width: 700px; margin: 36px auto 0px; text-align: center">
-       {!! $data['product_comparison_bottom'] !!}
+       {!! $productComparisondata['product_comparison_bottom'] !!}
     </div>
 </section>
 @endif
