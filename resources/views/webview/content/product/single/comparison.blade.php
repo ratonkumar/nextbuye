ad4e619a0b5f81186ca965eb4af27f80b1acd104
raton_kumar <ratankumarppi86@gmail.com>
2026-07-17 11:41:15 +0600

    comparision updated

--- resources/views/webview/content/product/single/comparison.blade.php
+++ resources/views/webview/content/product/single/comparison.blade.php
@@ -9,7 +9,7 @@
 @if($data)
 <section style="max-width: 940px; margin: 0px auto; padding: 80px 20px 20px">
    
-        {!! $data['comparison_title'] !!}
+        {!! $data['product_comparison_title'] !!}
     <p style="text-align: center; font-size: 16.5px; color: rgb(107, 100, 90); max-width: 560px; margin: 0px auto 36px; line-height: 1.75;">
         {!! $data['comparison_description'] !!}
     </p>
@@ -18,12 +18,12 @@
         
         <!-- Left Column (Generic) -->
         <div style="flex: 1 1 300px; max-width: 430px; background: rgb(247, 242, 234); border: 1px solid rgb(232, 224, 212); border-radius: 24px; padding: 26px 24px;">
-            {!! $data['comparison_left'] !!}
+            {!! $data['product_comparison_left'] !!}
         </div>
 
         <!-- Right Column (Choplet) -->
         <div style="flex: 1 1 300px; max-width: 430px; background: rgb(255, 255, 255); border: 2px solid rgb(240, 83, 43); border-radius: 24px; padding: 26px 24px; box-shadow: rgba(240, 83, 43, 0.55) 0px 26px 50px -30px; position: relative;">
-             {!! $data['comparison_right'] !!}
+             {!! $data['product_comparison_right'] !!}
         </div>
     </div>
 
