789e801bc61fc5d7b22145229a6eb0ef7eee868b
raton_kumar <ratankumarppi86@gmail.com>
2026-07-17 21:59:32 +0600

    updated

--- resources/views/webview/content/cart/complete.blade.php
+++ resources/views/webview/content/cart/complete.blade.php
@@ -9,7 +9,7 @@
         <div class="pt-5 pb-5" style="margin-bottom:5px">
             <div class="card py-3 mt-sm-3">
                 <div class="card-body text-center">
-                    <h2 class="h4 pb-3" style="color:green">আপনার অর্ডারটি সফলভাবে সম্পন্ন হয়েছে আমাদের কল সেন্টার থেকে ফোন করে আপনার অর্ডারটি কনফার্ম করা হবে</h2>
+                    <h2 class="h4 pb-3" style="color:green">আমরা আপনাকে বিস্বাসের।সাথে এই বলতে পারি আমরা।আপনাকে অরজিনাল অথেনটিক জেনুইন প্রডাক্টটি দিতে পারি। যা আপনি হাতে পেয়ে চেক করে নিবেন</h2>
                     <a class="btn btn-primary mt-3" href="{{url('/')}}">প্রোডাক্ট বাছাই করুন</a>
                 </div>
             </div>
