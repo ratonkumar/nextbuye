87a31207ad02532432a067f5444a4c936e186961
raton_kumar <ratankumarppi86@gmail.com>
2026-07-19 22:48:24 +0600

    details updated

--- resources/views/webview/content/product/single/details.blade.php
+++ resources/views/webview/content/product/single/details.blade.php
@@ -169,7 +169,7 @@
                             "
                             onclick="buynowDetails('{{ $productdetails->id }}')"
                         >
-                            {{ $top1['order_button_text'] ?? 'অর্ডার করুন — টাকা হাতে পেয়ে দেবেন' }}
+                            {!! $top1['order_button_text'] ?? 'অর্ডার করুন — টাকা হাতে পেয়ে দেবেন' !!}
                             
                             <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.4">
                                 <path d="M5 12h14M13 6l6 6-6 6"></path>
@@ -187,7 +187,7 @@
                         <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.8 19.8 0 0 1-8.63-3.07A19.5 19.5 0 0 1 4.69 12a19.8 19.8 0 0 1-3.07-8.67A2 2 0 0 1 3.6 1.27h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9a16 16 0 0 0 6.91 6.91l.61-.61a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
                     </svg>
                 </span>
-                {{ $top1['call_to_action']  }}
+                {!! $top1['call_to_action']  !!}
                  <span style="color: #b23a18">{{ $shipping->phone_one }}</span>
             </a>
 
